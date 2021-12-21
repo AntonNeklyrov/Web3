f639c9b95f2d33884ab06fa44bc5629a59192c80
Андрей Неклюдов <68616741+AntonNeklyrov@users.noreply.github.com>
2021-12-21 23:00:29 +0300

    Update signin.php

--- signin.php
+++ signin.php
@@ -11,14 +11,7 @@
     AND user_password='$_POST[user_password]'";
 
 
-    if(!$res = connect()->query($sql)){
-        echo "<script>alert('Данный пользователь не найден')</script>";
-        exit('<meta http-equiv="refresh" content="0; url=index.php" />');
-    }
 
-    $_SESSION['email'] = $_POST[user_email];
-    $_SESSION['name'] = $_POST[user_name];
-    $_SESSION['password'] = $_POST[user_password];
-    $_SESSION['sesia'] = 'true';
+  
 
 exit('<meta http-equiv="refresh" content="0; url=index.php" />');
